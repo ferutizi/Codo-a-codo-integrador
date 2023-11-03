200dab54f233328b0c0d81f5cebee8ff80870400
ferutizi <fer.utizi@gmail.com>
2023-11-03 16:56:48 -0300

    form.js totalPagar, clear form

---
+++ form.js
@@ -0,0 +1,36 @@
+const formulario = document.getElementById('formulario');
+const cantidad = formulario.amount;
+const categoria = formulario.category;
+const totalPago = document.getElementById('totalPago');
+const btnBorrar = document.getElementById('btnBorrar');
+const valor = 200;
+let descuento = 1;
+let total = 0;
+let span = document.createElement('span')
+
+btnBorrar.addEventListener('click', () => {
+    totalPago.removeChild(span)
+})
+
+function getTickets() {
+    switch (categoria.value) {
+        case '1':
+            /* 80% */
+            descuento = 0.2
+            break;
+        case '2':
+            /* 50% */
+            descuento = 0.5
+            break;
+        case '3':
+            /* 15% */
+            descuento = 0.85
+            break;
+        default:
+            descuento = 1
+            break;
+    }
+    total = valor * cantidad.value * descuento;
+    span.textContent = total
+    totalPago.appendChild(span)
+}
